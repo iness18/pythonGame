ad227eeb6e15048dfad3c9a099b91bedb4777085
Iness <iness@MacBook-Pro-de-Iness.local>
2021-01-27 10:32:41 +0100

    wip

--- comet.py
+++ comet.py
@@ -7,30 +7,15 @@ class Comet(pygame.sprite.Sprite):
         self.image = pygame.image.load("./assets/comet.png")
         self.rect = self.image.get_rect()
         self.velocity = random.randint(1, 3)
-<<<<<<< HEAD
-        self.rect.x = random.randint(20, 80)
-        self.rect.y = - random.randint(0, 800)
-        self.comet_event = comet_event
-=======
         self.rect.x = random.randint(20, 800)
         self.rect.y = - random.randint(20, 800)
         self.comet_event = comet_event
 
     def remove(self):
         self.comet_event.all_comet.remove(self)
->>>>>>> 4248a0bb57e95b467173f5f89bdbb38f7a5366d8
 
     def fall(self):
         self.rect.y += self.velocity
-
-<<<<<<< HEAD
-        if self.rect.y >= 500:
-            print("hooo mince !")
-            self.remove()
-
-
-
-=======
         # ne tombe pas au sol
         if self.rect.y >= 500:
             print("test")
@@ -45,4 +30,3 @@ class Comet(pygame.sprite.Sprite):
         if self.comet_event.game.check_collision(self, self.comet_event.game.all_player):
             self.remove()
             self.comet_event.game.player.damage(20)
->>>>>>> 4248a0bb57e95b467173f5f89bdbb38f7a5366d8
